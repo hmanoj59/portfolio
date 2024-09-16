445ced90dfa46e0c769a27a90cbfc8ffeacea3ca
Naga Hota <hotasmail@gmail.com>
2024-09-16 00:53:46 -0400

    react chgs

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Naga H</h1>
+        <p>Fixing Things</p>
+        <h2>About Me</h2>
+            <p>I'm a software and a techie, I would like to play and break things related to software, hardware and Gadgets.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
